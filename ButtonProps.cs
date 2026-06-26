63ec280172980df4c1e607517b2d862021a4a4bc
Ryiong <viethung1209.work@gmail.com>
2026-06-26 14:49:45 +0700

    Add project files.

---
+++ ButtonProps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace My_Library
+{
+    public static class ButtonProps
+    {
+        public static readonly DependencyProperty HoverBackgroundProperty =
+            DependencyProperty.RegisterAttached("HoverBackground", typeof(Brush), typeof(ButtonProps), new PropertyMetadata(Brushes.Transparent));
+
+        public static void SetHoverBackground(DependencyObject obj, Brush value) => obj.SetValue(HoverBackgroundProperty, value);
+        public static Brush GetHoverBackground(DependencyObject obj) => (Brush)obj.GetValue(HoverBackgroundProperty);
+
+        // 2. Thuộc tính Pressed Background
+        public static readonly DependencyProperty PressedBackgroundProperty =
+            DependencyProperty.RegisterAttached("PressedBackground", typeof(Brush), typeof(ButtonProps), new PropertyMetadata(Brushes.Transparent));
+
+        public static void SetPressedBackground(DependencyObject obj, Brush value) => obj.SetValue(PressedBackgroundProperty, value);
+        public static Brush GetPressedBackground(DependencyObject obj) => (Brush)obj.GetValue(PressedBackgroundProperty);
+    }
+}
---
+++ Main.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace My_Library
+{
+    /// <summary>
+    /// Interaction logic for Main.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+        }
+    }
+}
